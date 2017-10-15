8d974a29b837249b387a739cd01a46d13e4dfc44
Jani <github@posti.titityy.net>
2017-10-15 21:21:46 +0300

    Alustojen (pelikonsolit, tietokoneet) malli

---
+++ src/servlet/Servlet_HaeAlustat.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import com.sun.deploy.net.HttpRequest;
+import dao.Dao_Alusta;
+import dao.Dao_Artikkeli;
+import model.Alusta;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/Servlet_HaeAlustat")
+public class Servlet_HaeAlustat extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    public Servlet_HaeAlustat() {
+        super();
+        System.out.println("Servlet_HaeAlustat.Servlet_HaeAlustat()");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Servlet_HaeAlustat.doGet()");
+
+        Dao_Alusta dao = new Dao_Alusta();
+        try {
+            ArrayList<Alusta> alustat = dao.haeAlustat();
+            request.setAttribute("alustat", alustat);
+            String jsp = "/alustahaku.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
