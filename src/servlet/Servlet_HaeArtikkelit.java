4b9e9b3eab4fe64b7979e1309c78cb70627ed27e
Jani <github@posti.titityy.net>
2017-10-15 21:22:33 +0300

    Erotettu artikkelien servlet erilleen.

---
+++ src/servlet/Servlet_HaeArtikkelit.java
@@ -0,0 +1,39 @@
+package servlet;
+
+import dao.Dao_Artikkeli;
+import model.Artikkeli;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/Servlet_HaeArtikkelit")
+public class Servlet_HaeArtikkelit extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public Servlet_HaeArtikkelit () {
+        super();
+        System.out.println("Servlet_HaeArtikkelit.Servlet_HaeArtikkelit()");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Servlet_HaeArtikkelit.doGet()");
+
+        Dao_Artikkeli dao = new Dao_Artikkeli();
+        try {
+            ArrayList<Artikkeli> artikkelit = dao.haeArtikkelit();
+            request.setAttribute("artikkelit", artikkelit);
+            String jsp = "/artikkelihaku.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(jsp);
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
