5f475ea7dca739dfcd0d22430e7831201a7b827f
Jani <github@posti.titityy.net>
2017-10-16 20:57:38 +0300

    Korjattu metodin nimi.

--- src/dao/Dao_Artikkeli.java
+++ src/dao/Dao_Artikkeli.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Dao_Artikkeli extends Dao {
 
     public ArrayList<Artikkeli> haeArtikkelit() throws Exception {
-        ArrayList<Artikkeli> artikkelit= new ArrayList<Artikkeli>();
+        ArrayList<Artikkeli> artikkelit= new ArrayList<>();
         sql = "SELECT * FROM pm_artikkelit";
 //        sql = "SELECT alu.Nimi AS Alusta,art.Nimi,art.Lisatiedot,art.Nimi from pm_artikkelit_alustat pmaa join pm_artikkelit art on pmaa.Artikkelit_id = art.Artikkelit_id join pm_alustat alu on pmaa.Alustat_id = alu.Alustat_id ORDER BY alu.Nimi,art.Nimi"
         con = yhdista();
@@ -29,7 +29,7 @@ public class Dao_Artikkeli extends Dao {
         return artikkelit;
     }
 
-    public boolean lisaaArtikkelit(Artikkeli artikkeli) {
+    public boolean lisaaArtikkeli(Artikkeli artikkeli) {
         boolean paluuArvo = true;
 
         sql = "INSERT INTO pm_artikkelit(Nimi, Lisatiedot, Pyyntihinta) VALUES(?, ?, ?)";
