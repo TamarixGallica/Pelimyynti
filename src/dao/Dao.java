101892e398c866a8637f5ef6f53168dcf1eb68d0
Jani <github@posti.titityy.net>
2017-10-25 18:58:57 +0300

    Lisätty metodi tietojen hakemiseksi JSON-muodossa

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,9 +1,6 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class Dao {
     public Connection con = null;
@@ -24,5 +21,64 @@ public class Dao {
         if(con!=null) {
             con.close();
         }
+    }
+
+    public String haeTiedotJSON(String[] sarakkeet, String taulu) throws Exception {
+        String palautusJSON = "";
+        String sarakeStr = "";
+
+        // Ketjutetaan kaikki annetut sarakkeet pilkulla erotettuna paitsi viimeinen
+        for(int i=0; i < (sarakkeet.length-1); i++) {
+            sarakeStr += sarakkeet[i] + ",";
+        }
+        // Lisätään viimeinen sarake ilman pilkkua
+        sarakeStr += sarakkeet[sarakkeet.length-1];
+
+        sql = "SELECT " + sarakeStr + " FROM " + taulu;
+
+        System.out.println(sql);
+        con = yhdista();
+        if(con != null) {
+            stmtPrep = con.prepareStatement(sql);
+            rs = stmtPrep.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            if(rs!=null) {
+                int sarLkm = rsmd.getColumnCount();
+                palautusJSON += "[";
+                while (rs.next()) {
+                    palautusJSON += "{";
+                    for (int i = 1; i <= sarLkm; i++) {
+                        palautusJSON += "\"";
+                        palautusJSON += rsmd.getColumnName(i);
+                        palautusJSON += "\":";
+                        palautusJSON += "\"";
+                        try {
+                            palautusJSON += rs.getString(i);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        palautusJSON += "\"";
+                        if (i < sarLkm) {
+                            palautusJSON += ',';
+                        }
+                    }
+                    palautusJSON += "}";
+                    palautusJSON += ",";
+                }
+                palautusJSON += "]";
+            }
+
+            con.close();
+
+
+        }
+        //Siivotaan viimeinen pilkku pois
+        palautusJSON = palautusJSON.substring(0, palautusJSON.length()-2) + "]";
+
+        if(palautusJSON.length()==1){
+            palautusJSON="{}";
+        }
+
+        return palautusJSON;
     }
 }
