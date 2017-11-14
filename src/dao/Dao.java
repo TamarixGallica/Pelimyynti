59645009efd035fcd996ff8a97e514dd0e9c0615
Jani <github@posti.titityy.net>
2017-11-14 16:47:21 +0200

    JSON-lisäyksiä hakuja varten

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -23,7 +23,12 @@ public class Dao {
         }
     }
 
+
     public String haeTiedotJSON(String[] sarakkeet, String taulu) throws Exception {
+        return haeTiedotJSON(sarakkeet, taulu, "", "");
+    }
+
+    public String haeTiedotJSON(String[] sarakkeet, String taulu, String ehtoSarake, String ehtoArvo) throws Exception {
         String palautusJSON = "";
         String sarakeStr = "";
 
@@ -36,10 +41,69 @@ public class Dao {
 
         sql = "SELECT " + sarakeStr + " FROM " + taulu;
 
+        if(ehtoSarake.length()>0){
+            sql += " WHERE "+ehtoSarake+"=?";
+        }
+
         System.out.println(sql);
         con = yhdista();
         if(con != null) {
             stmtPrep = con.prepareStatement(sql);
+            if(ehtoSarake.length()>0){
+                stmtPrep.setString(1, ehtoArvo);
+            }
+            rs = stmtPrep.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            if(rs!=null) {
+                int sarLkm = rsmd.getColumnCount();
+                palautusJSON += "[";
+                while (rs.next()) {
+                    palautusJSON += "{";
+                    for (int i = 1; i <= sarLkm; i++) {
+                        palautusJSON += "\"";
+                        palautusJSON += rsmd.getColumnName(i);
+                        palautusJSON += "\":";
+                        palautusJSON += "\"";
+                        try {
+                            palautusJSON += rs.getString(i);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        palautusJSON += "\"";
+                        if (i < sarLkm) {
+                            palautusJSON += ',';
+                        }
+                    }
+                    palautusJSON += "}";
+                    palautusJSON += ",";
+                }
+                palautusJSON += "]";
+            }
+
+            con.close();
+
+
+        }
+        //Siivotaan viimeinen pilkku pois
+        palautusJSON = palautusJSON.substring(0, palautusJSON.length()-2) + "]";
+
+        if(palautusJSON.length()==1){
+            palautusJSON="{}";
+        }
+
+        return palautusJSON;
+    }
+
+    public String poistaTiedotJSON(String taulu, String ehtoSarake, String ehtoArvo) throws Exception {
+        String palautusJSON = "";
+
+        sql = "DELETE FROM " + taulu + " WHERE "+ ehtoSarake + "=?";
+        System.out.println(sql);
+
+        con = yhdista();
+        if(con != null) {
+            stmtPrep = con.prepareStatement(sql);
+            stmtPrep.setString(1, ehtoArvo);
             rs = stmtPrep.executeQuery();
             ResultSetMetaData rsmd = rs.getMetaData();
             if(rs!=null) {
